923b00ab511840832e933cdf71f344a652443fcd
Polina Sprinzhunas <polina.sprinzhunas@gmail.com>
2024-03-29 13:19:23 +0100

    HW7

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
@@ -14,6 +15,10 @@ namespace UnitBrains.Player
         private float _cooldownTime = 0f;
         private bool _overheated;
         private List<Vector2Int> _currentTargets = new List<Vector2Int>();
+        private static int _unitCounter = 0;
+        private int _unitNumber;
+        private const int MaxTargets = 3;
+
 
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
@@ -25,12 +30,12 @@ namespace UnitBrains.Player
             IncreaseTemperature();
 
             for (int i = 0; i <= temp; i++)
-            { 
+            {
                 var projectile = CreateProjectile(forTarget);
                 AddProjectileToList(projectile, intoList);
 
             }
-           
+
         }
 
 
@@ -41,8 +46,8 @@ namespace UnitBrains.Player
             else target = unit.Pos;
             if (IsTargetInRange(target)) return unit.Pos;
             else return target;
-        
-    }
+
+        }
 
         protected List<Vector2Int> GetAllTargets()
         {
@@ -54,46 +59,50 @@ namespace UnitBrains.Player
 
         protected override List<Vector2Int> SelectTargets()
         {
+            _currentTargets.Clear();
+            _currentTargets.AddRange(GetAllTargets());
 
-            List<Vector2Int> result = new List<Vector2Int>();
-            float minDistance = float.MaxValue;
-            Vector2Int closestTarget = Vector2Int.zero;
-
-                foreach (Vector2Int target in GetAllTargets())
-                {
-                    var distance = DistanceToOwnBase(target);
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    closestTarget = target;
-                }
-                }
-
-                _currentTargets.Clear();
-            if (minDistance < float.MaxValue)
-            {
-                _currentTargets.Add(closestTarget);
-                if (IsTargetInRange(closestTarget)) result.Add(closestTarget);
-
-            }
-            else
+            if (_currentTargets.Count == 0)
             {
                 _currentTargets.Add(runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerID : RuntimeModel.PlayerID]);
             }
 
-                return result;
+            SortTargetsByDistanceToBase();
+
+            Vector2Int targetPosition = DetermineTargetPosition();
+
+            if (IsTargetInRange(targetPosition))
+            {
+                return new List<Vector2Int> { targetPosition };
             }
+
+            return new List<Vector2Int>();
         }
 
-       
+        private void SortTargetsByDistanceToBase()
+        {
+            _currentTargets.Sort((a, b) =>
+            {
+                float distanceToA = Vector2Int.Distance(a, unit.Pos);
+                float distanceToB = Vector2Int.Distance(b, unit.Pos);
+                return distanceToA.CompareTo(distanceToB);
+            });
+        }
+
+        private Vector2Int DetermineTargetPosition()
+        {
+            _unitNumber = _unitCounter++;
+            int targetIndex = _unitNumber % Math.Min(MaxTargets, _currentTargets.Count);
+            return _currentTargets[targetIndex];
+        }
 
 
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -105,7 +114,7 @@ namespace UnitBrains.Player
 
         private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
 
@@ -115,3 +124,5 @@ namespace UnitBrains.Player
             if (_temperature >= OverheatTemperature) _overheated = true;
         }
     }
+
+}
